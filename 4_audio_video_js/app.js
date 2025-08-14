e036722aef398f75725c3b5a6d6a336cb7ebd72c
VILLAK <sebastianvillamil02@gmail.com>
2025-08-14 15:33:15 -0500

    audio y video parte 1

---
+++ 4_audio_video_js/app.js
@@ -0,0 +1,7 @@
+const video = document.querySelector('#video');
+const playVideo = document.querySelector('#play-video');
+const stopVideo = document.querySelector('#stop-video');
+const playStopImg1 = document.querySelector('#play-stop-img1');
+const playStopImg2 = document.querySelector('#play-stop-img2');
+
+console.log(video, playVideo, stopVideo, playStopImg1, playStopImg2);
