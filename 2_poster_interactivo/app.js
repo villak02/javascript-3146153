2229d8a0b08859d8c0f9f6a0398ff426414cf5a7
VILLAK <sebastianvillamil02@gmail.com>
2025-08-20 16:09:31 -0500

    Add files via upload

--- 2_poster_interactivo/app.js
+++ 2_poster_interactivo/app.js
@@ -4,6 +4,9 @@ const contadormonedas = document.querySelector('#contador-monedas');
 const contadorcorazones = document.querySelector('#contador-corazones');
 let totalmonedas = 0;
 let totalcorazones = 0;
+const audio1 = document.querySelector('#audio-1');
+const audio2 = document.querySelector('#audio-2');
+const audio3 = document.querySelector('#audio-3');
 
 /* console.log(monedas,corazon,contadormonedas,contadorcorazones) */
 
@@ -14,7 +17,12 @@ monedas.forEach( function(moneda) {
     totalmonedas++;
     contadormonedas.textContent = totalmonedas;
 
-    } )
+    if (audio1.paused) {
+        audio1.play();
+
+    } else {
+        audio1.pause();
+    }
 
     
 })
@@ -25,6 +33,13 @@ corazones.forEach( function(corazon){
         corazon.classList.add('saltar'); 
         totalcorazones++;
         contadorcorazones.textContent = totalcorazones;
+
+        if (audio2.paused) {
+            audio2.play();
+
+        } else {
+            audio2.pause();
+        }
     })
 })
 
