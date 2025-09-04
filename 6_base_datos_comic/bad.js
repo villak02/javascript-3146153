efb7a8688c5ea6eac3c7c7ebb28fe81642639364
VILLAK <sebastianvillamil02@gmail.com>
2025-09-04 16:53:25 -0500

    pagina terminada

---
+++ 6_base_datos_comic/bad.js
@@ -0,0 +1,58 @@
+const comic = {
+
+"nombreComic": "libralos del mal",
+"categoria": "apocalipsis",
+"cantidadCap": 4,
+"descripcion": " la llegada de cristo esta presente y la vida de kael cabia repentinamete, mostrandod el punto de vista de los malditos",
+"personajes": ["pepa", "lucy", "mario" "pastor"],
+"capítulos":[
+
+{
+
+nombreCap: "libranos del mal, amen.",
+imgPortada: "img/portada1.jpg",
+video: "video/cap1.mp4",
+descripcion: "kael, su esposa sandy y su hijo kael, suelen ir a la iglesia y evidentemente se hacen ver como una familia religiosa y creyente del cristianismo",
+personajes: ["kael", "kyle", "sandy"]
+
+},
+{
+nombreCap: "suplicas de ayuda",
+imgPortada: "img/portada2.jpg",
+video: "video/cap2.mp4",
+descripcion: "el matriomonio de kael y sandy cada vez esa mas roto, aun asi, los tres integrantes de la familia viven un duelo silencioso  ",
+personajes: ["kael", "sandy", "kyle"]
+
+}
+
+{
+
+nombreCap: "profecias de fantasia",
+imgPortada: "img/portada1.jpg",
+video: "video/cap1.mp4",
+descripcion: "suenan trompetas, gritos y relampagos, sandy y kyle han desaparecido y han dejado su ropa tiratda, kael hara todo por encontrarlos y entender que es lo que esta pasando",
+personajes: ["kael", "pastor",]
+
+},
+
+{
+
+nombreCap: "No hablar con desconocidos",
+imgPortada: "img/portada1.jpg",
+video: "video/cap1.mp4",
+descripcion: "el miedo invade a kael, aun se niega aceptar el paradero de su familia y solo busca un lugar en donde refugiarse",
+personajes: ["kael", "hombre herido" , "satanas"]
+
+},
+
+{
+
+nombreCap: "La hora del juicio",
+imgPortada: "img/portada1.jpg",
+video: "video/cap1.mp4",
+descripcion: "el miedo consume a kael, y aunque su destino es dificil de aceptar pierde todo tipo de esperanzas",
+personajes: ["kael", "kyle", "sandy", "satanas"]
+
+},
+]
+}
